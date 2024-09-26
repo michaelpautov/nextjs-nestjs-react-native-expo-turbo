ee797db1da849f799bdcb2622814983c52afac5e
Michael <michael.pautov@gmail.com>
2024-09-26 20:46:21 +0600

    feat(packages/constants): implemented (#9)

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,3 +1,5 @@
+import { WEBAPP_CONSTANTS } from "@app/constants";
+
 export default function HomePage() {
-  return <div>hello world</div>;
+  return <div>hello world {WEBAPP_CONSTANTS.TEST_CONSTANTS}</div>;
 }
